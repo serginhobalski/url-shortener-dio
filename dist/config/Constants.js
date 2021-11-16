15617070a02162b3012b7959c7efda738e5626ce
Sergio Obalski Filho <serginhobalski@gmail.com>
2021-11-16 18:27:29 -0300

    Inicio

---
+++ dist/config/Constants.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION: 'mongodb+srv://serginhobalski:<password>@cluster0.jjie1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+};
+//# sourceMappingURL=Constants.js.map
---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://serginhobalski:<password>@cluster0.jjie1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
